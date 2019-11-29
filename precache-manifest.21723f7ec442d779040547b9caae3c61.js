199943bcadadf23512f569f2e7144e492747cd90
Dmitriymush <powder322@gmail.com>
2019-11-29 02:49:47 +0200

    Updates

---
+++ precache-manifest.21723f7ec442d779040547b9caae3c61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ebb3df9636a0d246c54500a9810e95",
+    "url": "/js_changing-clothes/index.html"
+  },
+  {
+    "revision": "8e1d0673e2605d294187",
+    "url": "/js_changing-clothes/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "c55d74a42bd04e46e144",
+    "url": "/js_changing-clothes/static/js/2.8445cdec.chunk.js"
+  },
+  {
+    "revision": "8e1d0673e2605d294187",
+    "url": "/js_changing-clothes/static/js/main.23d434c6.chunk.js"
+  },
+  {
+    "revision": "e784382c251c899f6809",
+    "url": "/js_changing-clothes/static/js/runtime-main.57a21063.js"
+  }
+]);
